b88556870c969f2eb498c9ae8fa8bc0e99ece2a7
Tami Churns <tchurns@paperlesspost.com>
2019-03-07 00:19:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 /* Enter the code to remove the main node element under this comment */
 
+let myMain = document.querySelector ('main')
+myMain.remove()
+
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerText = "Tami is the champion!"
+document.body.appendChild(newHeader)
+
